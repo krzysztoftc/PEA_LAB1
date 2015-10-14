eca8559fa631bfe95e50b5e55b2c546347f73a93
krzysztoftc <krzysztoftc@vp.pl>
2015-10-14 14:12:53 +0200

    Pierwsze starcie

---
+++ src/PEA_LAB1.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : PEA_LAB1.cpp
+// Author      : Kinga & Krzysztof
+// Version     :
+// Copyright   : Copyright 2015: Kinga Wilczek 210063 & Krzysztof Cabala 210047 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Zaczynamy!!!" << endl; // prints !!!Zaczynamy!!!
+	return 0;
+}
