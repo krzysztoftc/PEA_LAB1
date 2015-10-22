d45e4fad9b1d7345f5b972f9188eb761c778d6fe
krzysztoftc <krzysztoftc@vp.pl>
2015-10-22 18:02:44 +0200

    SolutionNode

---
+++ src/SolutionNode.h
@@ -0,0 +1,24 @@
+/*
+ * SolutionNode.h
+ *
+ *  Created on: 22 paź 2015
+ *      Author: krzysztof
+ */
+
+#ifndef SOLUTIONNODE_H_
+#define SOLUTIONNODE_H_
+
+#include <list>
+
+struct SolutionNode{
+	MatrixCosts matrix;
+	int lowBound;
+	SolutionNode leftSon;
+	SolutionNode rightSon;
+	SolutionNode parent;
+	std::list<int> trace;
+};
+
+
+
+#endif /* SOLUTIONNODE_H_ */
