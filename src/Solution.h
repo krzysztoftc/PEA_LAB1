c5de8e2d01695048d39bc47917a7b9238f10a2e8
krzysztoftc <krzysztoftc@vp.pl>
2015-11-06 13:58:05 +0100

    Soultion struct

---
+++ src/Solution.h
@@ -0,0 +1,18 @@
+/*
+ * Solution.h
+ *
+ *  Created on: 6 lis 2015
+ *      Author: krzysztof
+ */
+
+#ifndef SOLUTION_H_
+#define SOLUTION_H_
+
+
+struct Solution{
+	std::list<int> trace;
+	int cost;
+};
+
+
+#endif /* SOLUTION_H_ */
